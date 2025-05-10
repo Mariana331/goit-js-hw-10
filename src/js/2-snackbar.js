56ffa388f76ad0f10ace479c1cdeca5d9fca097b
Mariana_k <m.koval000@gmail.com>
2025-05-10 11:10:25 +0300

    update

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,40 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const formElem = document.querySelector(`.form`);
+
+formElem.addEventListener(`submit`, event => {
+  event.preventDefault();
+
+  const delay = event.target.elements.delay.value;
+  const state = event.target.elements.state.value;
+
+  createPromise(delay, state)
+    .then(delay => {
+      пше;
+      iziToast.success({
+        title: '✅ Success',
+        message: `Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: '❌ Error',
+        message: `Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+});
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
